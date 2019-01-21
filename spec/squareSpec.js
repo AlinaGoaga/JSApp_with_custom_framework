40bba73ff5e6389c1a1129688787b6e398c03592
Alina Goaga <goaga.alina@gmail.com>
2019-01-21 22:07:43 +0000

    Refactored to use the expect(x).to(be/do)something(y) type of syntax

--- spec/squareSpec.js
+++ spec/squareSpec.js
@@ -1,35 +1,19 @@
 var square = new Square(2);
 
-//green
 describe('area', () => {
   it('calculates the area of a square', () => {
-    expectEquality(square.area(), 4)
-  })
-})
-
-//red
-describe('area', () => {
-  it('calculates the area of a square', () => {
-    expectEquality(square.area(), 5)
-  })
-})
-
-//green
-describe('area', () => {
-  it('calculates the area of a square', () => {
-    expectInequality(square.area(), 5)
-  })
-})
-
-//red
-describe('area', () => {
-  it('calculates the area of a square', () => {
-    expectInequality(square.area(), 4)
+    expect(square.area()).toEqual(4)
   })
 })
 
 describe('constructor', () => {
   it('only accepts numbers', () => {
-     checkError(function() { new Square("r"); });
+     expect(function() { new Square("r"); }).toThrowError('argument is not a number');
+  })
+})
+
+describe('constructor', () => {
+  it('does not accept negative values', () => {
+     expect(function() { new Square(-19); }).toThrowError('size should be a positive number');
   })
 })
